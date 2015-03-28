4c8fb369fecf29ec5907afa27e9ad2959e103f5d
pmallya <pmallya@users.noreply.github.com>
2015-03-28 12:15:46 -0700

    Updated priority encoder
    This priority behavioral gives least gates on synthesis.

--- priority_encoder_behavioral.v
+++ priority_encoder_behavioral.v
@@ -1,47 +1,20 @@
-module prority_encoder (
-	input [7:0] A,
-    output [7:0] B);
-	
-	reg [3:0] pos;
-	reg [7:0] B;
-	
-/*	reg [7:0] C;
-	wire temp;
-	reg [3:0]counter;
-	assign temp = A[7];
+// Priority encoder
+module priority_encoder (A, Y);
+input [7:0] A; // 8-bit input vector
+output [7:0] Y; // 8-bit encoded output
+reg [7:0] Y; // target of assignment
 
-	always @*  begin
-	 while((pos >= 0) && ~temp) begin
-		C = A << 1;
-		temp = C[7];
-		pos = pos - 1;
-	 end 
-	 end
-end	   */
-
-always @* begin
-	if (A[7] == 1) pos = 8'd7;
-	else if (A[6] == 1) pos = 8'd6;
-	else if (A[5] == 1) pos = 8'd5;
-	else if (A[4] == 1) pos = 8'd4;
-	else if (A[3] == 1) pos = 8'd3;
-	else if (A[2] == 1) pos = 8'd2;
-	else if (A[1] == 1) pos = 8'd1;
-	else if (A[0] == 1) pos = 8'd0;
-end
-	
-always @* begin	
-	case(pos)
-		 0: B = 8'd1;
-		 1: B = 8'd2;
-		 2: B = 8'd4;
-		 3: B = 8'd8;
-		 4: B = 8'd16;
-		 5: B = 8'd32;
-		 6: B = 8'd64;
-		 7: B = 8'd128;
-    	default: B = 8'd0;
-	endcase
-end
-
+always @(A)
+ case(1'b1)
+ A[7]: Y = 8'd128;
+ A[6]: Y = 8'd64;
+ A[5]: Y = 8'd32;
+ A[4]: Y = 8'd16;
+ A[3]: Y = 8'd8;
+ A[2]: Y = 8'd4;
+ A[1]: Y = 8'd2;
+ A[0]: Y = 8'd1;
+ default: Y = 8'd0; // Don?t care when input is all 0?s
+ //default: Y = 8'bxxxxxxxx; // Don?t care when input is all 0?s
+ endcase
 endmodule
