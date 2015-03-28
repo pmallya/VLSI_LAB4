40363f0cd63a4061a8f30b92e66fa93ffc11770e
pmallya <pmallya@users.noreply.github.com>
2015-03-28 14:13:04 -0700

    Added assertions
    assertions check if output is one hot  or has Xor Z

--- tb_priority_ripple.v
+++ tb_priority_ripple.v
@@ -1,14 +1,32 @@
 module tb_priority_ripple;
-	reg [7:0] A;
-	wire [7:0] Y1;
+	logic [7:0] A;
+	logic [7:0] Y1;
 			   	
 	pr_encoder_ripple X1( .A(A),.Y(Y1));							  	
-			   	
+		
+  assert property (always@(A) $onehot(Y1))  else $error("Error: Your output Y=%b is not onehot", Y1);
+  assert property (always@(A) ~$isunknown(Y1)) else $error("Error: Your output Y=%b has X or Z", Y1);
+                        
 	initial begin  
-		repeat(21) begin 
-		A = $random ;
+	                 
+      		A = 8'b0;
 		#1 $monitor("A = %b	Y = %b",A, Y1);
-		end
+      
+		repeat(21) begin 
+			#1 A = $random ;
+			#1 $monitor("A = %b	Y = %b",A, Y1);
+		end		
+		
+		#1 A = 8'bx;
+		#1 $monitor("A = %b	Y = %b",A, Y1);
+		
+		#1 A = 8'bz;
+		#1 $monitor("A = %b	Y = %b",A, Y1);
+		
+		#1 A = 8'b0;
+		#1 $monitor("A = %b	Y = %b",A, Y1);
+         
+                       
 	end
 
 endmodule
